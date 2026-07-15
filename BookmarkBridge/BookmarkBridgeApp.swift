a1ed5cbf4e3e0e67d94364bdb75bdea99a9af81b
Jérôme Hudecek <jerome.hudecek@gmail.com>
2026-07-15 12:26:48 +0200

    Initial Commit

---
+++ BookmarkBridge/BookmarkBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookmarkBridgeApp.swift
+//  BookmarkBridge
+//
+//  Created by Jerome on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct BookmarkBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookmarkBridgeTests/BookmarkBridgeTests.swift
@@ -0,0 +1,18 @@
+//
+//  BookmarkBridgeTests.swift
+//  BookmarkBridgeTests
+//
+//  Created by Jerome on 15/07/2026.
+//
+
+import Testing
+
+struct BookmarkBridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
